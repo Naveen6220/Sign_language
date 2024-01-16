ec273c49681bbb3030bda76cb382ae55effb9418
Naveen6220 <nivinkumar7197@gmail.com>
2024-01-17 01:03:09 +0530

    Data validation added

--- sign_language/pipeline/train_pipeline.py
+++ sign_language/pipeline/train_pipeline.py
@@ -2,20 +2,23 @@ import sys, os
 from sign_language.logger import logging
 from sign_language.exception import SignException
 from sign_language.components.data_ingestion import DataIngestion
+from sign_language.components.data_validation import DataValidation
+
 
 
 
 from sign_language.entity.config_entity import (DataIngestionConfig,
-                                                )
+                                                DataValidationConfig)
 
 
 from sign_language.entity.artifact_entity import (DataIngestionArtifact,
-                                                )
+                                                DataValidationArtifact)
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config = DataValidationConfig()
 
 
     def start_data_ingestion(self)-> DataIngestionArtifact:
@@ -39,6 +42,32 @@ class TrainPipeline:
 
         except Exception as e:
             raise SignException(e, sys)
+        
+
+
+    def start_data_validation(
+        self, data_ingestion_artifact: DataIngestionArtifact
+    ) -> DataValidationArtifact:
+        logging.info("Entered the start_data_validation method of TrainPipeline class")
+
+        try:
+            data_validation = DataValidation(
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_config=self.data_validation_config,
+            )
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                "Exited the start_data_validation method of TrainPipeline class"
+            )
+
+            return data_validation_artifact
+
+        except Exception as e:
+            raise SignException(e, sys) from e
          
 
 
