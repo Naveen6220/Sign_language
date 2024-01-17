968413ad1e563ff2fcfa3f06beb4d13ff77a7d99
Naveen6220 <nivinkumar7197@gmail.com>
2024-01-18 01:14:21 +0530

    Flask app added

--- sign_language/components/model_trainer.py
+++ sign_language/components/model_trainer.py
@@ -37,10 +37,11 @@ class ModelTrainer:
             with open(f'yolov5/models/custom_{model_config_file_name}.yaml', 'w') as f:
                 yaml.dump(config, f)
 
-            os.system(f"cd yolov5/ && python train.py --img 416 --batch {self.model_trainer_config.batch_size} --epochs {self.model_trainer_config.no_epochs} --data ../data.yaml --cfg ./models/custom_yolov5s.yaml --weights {self.model_trainer_config.weight_name} --name yolov5s_results  --cache")
-            os.system("cp yolov5/runs/train/yolov5s_results/weights/best.pt yolov5/")
+            os.system(f"python yolov5/train.py --img 416 --batch {self.model_trainer_config.batch_size} --epochs {self.model_trainer_config.no_epochs} --data ../data.yaml --cfg ./models/custom_yolov5s.yaml --weights {self.model_trainer_config.weight_name} --name yolov5s_results --cache --nosave")
+            os.system(f"cp yolov5/runs/train/yolov5s_results/weights/best.pt yolov5/")
             os.makedirs(self.model_trainer_config.model_trainer_dir, exist_ok=True)
             os.system(f"cp yolov5/runs/train/yolov5s_results/weights/best.pt {self.model_trainer_config.model_trainer_dir}/")
+
            
             os.system("rm -rf yolov5/runs")
             os.system("rm -rf train")
